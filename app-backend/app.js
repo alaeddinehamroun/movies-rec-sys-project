0571b3f509c20455432437ae3ebd48fa17b9e63d
alaeddinehamroun <alaeddinehamroun@insat.u-carthage.tn>
2023-05-07 12:45:31 +0100

    added querying movies and ratings

--- app-backend/app.js
+++ app-backend/app.js
@@ -1,37 +1,98 @@
-import Kafka from 'node-rdkafka'; // Node.js wrapper for Kafka C/C++ library
-
-
-
-const stream = Kafka.Producer.createWriteStream({
-  'metadata.broker.list': '172.20.0.2:9092'
-}, {}, { topic: 'ratings-topic' }
-);
-
-
 import express from 'express';
 const app = express();
-app.use(express.json()); // Parse JSON requests
 
+import Kafka from 'node-rdkafka'; // Node.js wrapper for Kafka C/C++ library
+import hbase from 'hbase';
+
+
+
+// const stream = Kafka.Producer.createWriteStream({
+//   'metadata.broker.list': '172.20.0.2:9092'
+// }, {}, { topic: 'ratings-topic' }
+// );
+
+
+
+app.use(express.json()); // Parse JSON requests
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 /**
  * POST /ratings
  * Purpose: Add a rating
  */
 app.post('/ratings', (req, res) => {
 
-  console.log(req.body)
+  //console.log(req.body)
   const message = JSON.stringify(req.body);
 
-  const success = stream.write(Buffer.from(message));
+  // const success = stream.write(Buffer.from(message));
 
-  if (success) {
-    console.log("Message sent successfully to topic:", message);
-  } else {
-    console.log("Something went wrong.")
-  }
+  // if (success) {
+  //   console.log("Message sent successfully to topic:", message);
+  // } else {
+  //   console.log("Something went wrong.")
+  // }
 
-  res.send(message)
+  res.send(req.body)
 
 });
+
+
+
+
+
+
+const client = hbase({ host: '172.22.0.5', port: 8080 })
+
+
+
+/**
+ * GET: /ratings:uesrId
+ * Purpose: Get ratings by user
+ */
+app.get('/ratings/:userId', (req, res) => {
+
+
+
+  const userId = req.params.userId
+
+  // const myScanner = new hbase.Scanner(client, {table: 'ratings'})
+  const table = client.table('ratings');
+  table.scan({
+    filter: {
+      "op": "MUST_PASS_ALL", "type": "FilterList", "filters": [{
+        "op": "EQUAL",
+        "type": "RowFilter",
+        "comparator": { "value": `^${userId}$`, "type": "RegexStringComparator" }
+      }
+      ]
+    }
+  }, (error, cells) => {
+    if (error) {
+      res.status(500).send('Internal Server Error');
+    }
+    res.send(cells)
+  });
+  // client.table("ratings").row('1').get( (error, value) => {
+  //   console.info(value)
+
+  // })
+
+  // ratings = [
+  //   { user_id: 123, movie_id: 1, rating: 4.5 },
+  //   { user_id: 123, movie_id: 1, rating: 4.5 },
+  //   { user_id: 123, movie_id: 1, rating: 4.5 },
+  //   { user_id: 223, movie_id: 1, rating: 4.5 },
+  //   { user_id: 223, movie_id: 1, rating: 4.5 },
+  //   { user_id: 223, movie_id: 1, rating: 4.5 },
+  // ]
+
+  // send ratings where user_id == userId
+
+})
 
 /**
  * GET /recommendations/:userId
@@ -40,7 +101,55 @@ app.post('/ratings', (req, res) => {
 app.get('/recommendations/:userId', (req, res) => {
 
 
-  
+
+})
+
+
+
+/**
+ * GET /movies/:movieId
+ * Purpose: Get movie by id
+ */
+// TODO: NOT TESTED
+// app.get('/movies/:movieId', (req, res) => {
+
+//   const movieId = req.params.movieId
+
+
+//   const table = client.table('movies');
+//   table.scan({
+//     filter: {
+//       "op": "MUST_PASS_ALL", "type": "FilterList", "filters": [{
+//         "op": "EQUAL",
+//         "type": "RowFilter",
+//         "comparator": { "value": `^${movieId}$`, "type": "RegexStringComparator" }
+//       }
+//       ]
+//     }
+//   }, (error, rows) => {
+//     if (error) {
+//       res.status(500).send('Internal Server Error');
+//     }
+//     res.send(rows)
+//   });
+// })
+
+/**
+ * GET /movies
+ * Purpose: Get all movies
+ */
+app.get('/movies', (req, res) => {
+
+
+  const table = client.table('movies');
+  table.scan({
+
+  }, (error, rows) => {
+    if (error) {
+      res.status(500).send('Internal Server Error');
+    }
+    res.send(rows)
+  });
 })
 
 
